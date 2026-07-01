1803ee483ecc6d53577a8c8e143ea674d2df8491
Sun Less <Siddharthashahthakuri447@gmail.com>
2026-07-02 00:09:42 +0100

    fix: remove unused discord routes from auth.routes.ts

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { signup, login, discordLogin, discordCallback } from "../controllers/auth.controller";
+import { signup, login } from "../controllers/auth.controller";
 
 const router = Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/discord/login", discordLogin);
-router.get("/discord/callback", discordCallback);
 
 export default router;
